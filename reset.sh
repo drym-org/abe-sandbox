68f701d864bdf5edb2465b042ce81e2e71856f38
Siddhartha <sid@countvajhula.com>
2023-05-02 17:27:09 -0700

    Script to reset the ABE state of the repo to a given template

---
+++ reset.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    >&2 echo "No arguments provided!"
+    exit 1
+fi
+
+echo "Resetting to template: $1 ..."
+rm -rf abe/*
+cp -R templates/$1/* abe/
+git add -A abe/*
+git commit -m "Restore template: $1"
+echo "... done."
