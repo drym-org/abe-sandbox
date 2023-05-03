9a7e10cfff471fe08d34ee4cb61a0d38131ae755
Siddhartha <sid@countvajhula.com>
2023-05-02 20:17:45 -0700

    handle missing template

--- reset.sh
+++ reset.sh
@@ -5,9 +5,16 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+if [ ! -d templates/$1 ]
+then
+    echo "Directory templates/$1 does not exist!"
+    exit 1
+fi
+
 echo "Resetting to template: $1 ..."
 rm -rf abe/*
 cp -R templates/$1/* abe/
 git add -A abe/*
 git commit -m "Restore template: $1"
 echo "... done."
+
